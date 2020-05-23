57c09ea091a2444275da48be1c9b04826a97786e
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-05-23 12:45:53 +0200

    Russian translation

---
+++ locale/StoryTeller.ru.lua
@@ -0,0 +1,25 @@
+if ( GetLocale() == "ruRU" ) then
+	StoryTeller.Msg.STARTUP = "StoryTeller v{version}."
+
+	StoryTeller.Msg.PASTE_TEXT = "Вставьте текст сюда..."
+	StoryTeller.Msg.TELL_A_STORY = "Рассказать историю"
+	StoryTeller.Msg.CLEAR = "Очистить"
+
+	StoryTeller.Msg.READ = "Прочитано"
+	StoryTeller.Msg.PREV = "<<"
+	StoryTeller.Msg.NEXT = ">>"
+
+	StoryTeller.Msg.EDIT = "Редактировать"
+	StoryTeller.Msg.EDIT_TEXT = "Редактировать текст"
+	StoryTeller.Msg.SAVE = "Сохранить"
+	StoryTeller.Msg.CANCEL = "Отмена"
+
+	StoryTeller.Msg.PLAYER_TOOLTIP_VERSION = "StoryTeller v{version}"
+	StoryTeller.Msg.TOOLTIP_LEFT_CLICK = "**ЛКМ**: {action}"
+	StoryTeller.Msg.TOOLTIP_RIGHT_CLICK = "**ПКМ**: {action}"
+	StoryTeller.Msg.TOOLTIP_DRAG_AND_DROP = "**Перетаскивание** для перемещения кнопки"
+	StoryTeller.Msg.TOOLTIP_ACTION_SHOW_MAIN_WINDOW = "Показать главное окно"
+	StoryTeller.Msg.TOOLTIP_ACTION_HIDE_MAIN_WINDOW = "Скрыть главное окно"
+	StoryTeller.Msg.TOOLTIP_ACTION_SHOW_EDIT_WINDOW = "Открыть редактор"
+	StoryTeller.Msg.TOOLTIP_ACTION_HIDE_EDIT_WINDOW = "Закрыть редактор"
+end
