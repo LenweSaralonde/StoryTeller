77028b1f8043702218b3e6d2c51665940e7a7e11
LenweSaralonde <lenwe@lenwe.info>
2020-12-14 22:58:02 +0100

    Normalized function declarations

--- ui/StoryTellerEditFrame.lua
+++ ui/StoryTellerEditFrame.lua
@@ -1,4 +1,4 @@
-StoryTellerEditFrame.Init = function()
+function StoryTellerEditFrame.Init()
 	StoryTellerEditFrameTitle:SetText(StoryTeller.Msg.EDIT_TEXT)
 	StoryTellerEditFrameClearButton:SetText(StoryTeller.Msg.CLEAR)
 	StoryTellerEditFrameCancelButton:SetText(StoryTeller.Msg.CANCEL)
@@ -8,18 +8,18 @@ StoryTellerEditFrame.Init = function()
 	StoryTeller.SetButtonTextSize(StoryTellerEditFrameSaveButton)
 end
 
-StoryTellerEditFrame.Clear = function()
+function StoryTellerEditFrame.Clear()
 	StoryTellerEditFrameText:SetText("")
 	StoryTellerEditFrameScrollFrame:SetVerticalScroll(0)
 	StoryTellerEditFrameText:SetFocus()
 end
 
-StoryTellerEditFrame.Save = function()
+function StoryTellerEditFrame.Save()
 	StoryTellerFrameText:SetText(StoryTellerEditFrameText:GetText())
 	StoryTellerFrame.Load()
 	StoryTellerEditFrame:Hide()
 end
 
-StoryTellerEditFrame.Cancel = function()
+function StoryTellerEditFrame.Cancel()
 	StoryTellerEditFrame:Hide()
 end
